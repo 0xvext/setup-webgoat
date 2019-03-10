63bb58788be23607e438c58ffaf9304b88c7d56d
0xvext <0xvext@users.noreply.github.com>
2019-03-10 03:24:59 -0400

    Update setup-webgoat.sh

--- setup-webgoat.sh
+++ setup-webgoat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Install screen and openjdk 11
+# Install add-apt-repository, screen, and openjdk 11
 apt update
 apt install software-properties-common screen -y
 add-apt-repository ppa:openjdk-r/ppa -y
 apt update
 apt install openjdk-11-jdk -y
 
-# Download and run WebGoat
+# Download and run WebGoat (change IP to the interface to serve on)
 wget https://github.com/WebGoat/WebGoat/releases/download/v8.0.0.M24/webgoat-server-8.0.0.M24.jar
 screen java -jar ./webgoat-server-8.0.0.M24.jar --server.address=192.168.169.127
