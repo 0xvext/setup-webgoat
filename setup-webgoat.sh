ad741484b7a9fac39bf86369e44bc5fd2ccabf8d
0xvext <0xvext@users.noreply.github.com>
2019-03-10 01:20:49 -0500

    Update setup-webgoat.sh

--- setup-webgoat.sh
+++ setup-webgoat.sh
@@ -9,4 +9,4 @@ apt install openjdk-11-jdk -y
 
 # Download and run WebGoat
 wget https://github.com/WebGoat/WebGoat/releases/download/v8.0.0.M24/webgoat-server-8.0.0.M24.jar
-screen java -jar ./webgoat-server-8.0.0.M24.jar
+screen java -jar ./webgoat-server-8.0.0.M24.jar --server.address=192.168.169.127
